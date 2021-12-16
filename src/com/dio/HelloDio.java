c99bb083b32cd34c490360923a7d2cf7bb8ba5f5
BrendoSilva <brendosilvaxxt@gmail.com>
2021-12-16 15:15:03 -0300

    first commit

---
+++ src/com/dio/HelloDio.java
@@ -0,0 +1,21 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class HelloDio {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato(null, null, null);
+		Livro livro = new Livro();
+			
+		System.out.println(gato);
+		System.out.println(livro);
+
+	}
+
+}
+
+class Livro {
+	private String nome;
+	private String npag;
+}
